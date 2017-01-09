c924135df110618f32daa10afb8ba2907773ea7a
seraprog <seraphin.prog.info@gmail.com>
2017-01-09 07:03:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-test
+<?php
+include_once('modele/connexion_sql.php');
+
+if (!isset($_REQUEST['section']))
+{
+    include_once('controleur/annuaire/index.php');
+}
+
+if (isset($_REQUEST['section']) && $_REQUEST['section'] == 'categorie')
+{
+    include_once('controleur/annuaire/categorie.php');
+}
+
+if (isset($_REQUEST['section']) && $_REQUEST['section'] == 'fiche')
+{
+    include_once('controleur/annuaire/fiche.php');
+}
+if (isset($_REQUEST['section']) && $_REQUEST['section'] == 'recherche')
+{
+    include_once('controleur/annuaire/recherche.php');
+}
+?>
